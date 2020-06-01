f2224d3ec32b352d6e5a4967a6f13637aad9ef9c
ayodele-ademeso <ademesoayodele@gmail.com>
2020-06-01 20:28:32 +0100

    created the static folders and and templates for blog app

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class Blog(models.Model):
     pubdate = models.DateTimeField()
     body = models.TextField()
     postmedia = models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.body[:80]
+
+    def pubdate_pretty(self):
+        return self.pubdate.strftime('%b %e %Y')
